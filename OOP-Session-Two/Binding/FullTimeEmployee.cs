c536435fb0c94f07b182c922671872c5274ee76a
Abdelrahman Mansour741 <abdo.mansour741@gmail.com>
2025-07-22 23:32:51 +0300

    Demo-02

---
+++ OOP-Session-Two/Binding/FullTimeEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Session_Two.Binding
+{
+    internal class FullTimeEmployee : Employee
+    {
+       
+        public double Salary { get; set; }
+
+        public new void Fun01()
+        {
+            Console.WriteLine("I am An FullTimeEmployee");
+        }
+        public override void Fun02()
+        {
+            Console.WriteLine($"Salay : {Salary}");
+        }
+    }
+}
